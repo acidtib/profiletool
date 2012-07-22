e381512146fb065076c243f3ff9780b9e7ea133c
Thomas Vendetta <thomas@thomasvendetta.com>
2012-07-21 23:22:22 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thomasite::Application.config.secret_token = 'cb2122a80a519dab2983fca019c68ec99bb741fe043eaea99d4ab4a191149dd07f2c835a254249b6b6badc264b56ee3e4af2aa1d87a88793f4ac23dcea1042e7'
