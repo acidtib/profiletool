7e4d5385192dd050e53e20d65bc170ff47c3abe6
Thomas Vendetta <thomas@thomasvendetta.com>
2012-07-22 00:33:17 -0400

    Changez

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,11 @@ set :branch, :master
 set :deploy_to, '/var/www/rails/thomaswebsite'
 set :deploy_via, :remote_cache
 set :scm, :git
-set :user, "app"
+set :user, "thomas"
+
+set :default_environment, {
+  'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
+}
 
 set :rails_env, :production
 set :unicorn_binary, "/home/app/.rbenv/shims/unicorn"
@@ -13,8 +17,6 @@ set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
 
 ssh_options[:forward_agent] = true
 default_run_options[:pty] = true 
-
-
 
 role :web, "thomasvendetta.com"                          # Your HTTP server, Apache/etc
 role :app, "thomasvendetta.com"                          # This may be the same as your `Web` server
