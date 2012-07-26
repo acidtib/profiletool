f51631e23a67efc6cf1c0d9c5ebab3f822a2a4c4
Thomas Vendetta <thomas@thomasvendetta.com>
2012-07-26 18:59:51 -0400

    Adding untracked and changed form

---
+++ app/controllers/dashboard/dashboard_controller.rb
@@ -0,0 +1,17 @@
+class Dashboard::DashboardController < ApplicationController
+
+  before_filter :authenticate_user!
+ 
+  def index
+    if current_user.website.nil?
+      redirect_to new_dashboard_website_path, :notice => "Setup your website!"
+    else
+      @website = current_user.website
+      if @website.website_pages_enabled?
+        @pages = @website.website_pages.all
+      end
+    end
+  end
+
+end
+
