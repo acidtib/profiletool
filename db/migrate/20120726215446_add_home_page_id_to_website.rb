52b61a386a57d41272b2c11f79cc329f0bd9af56
Thomas Vendetta <thomas@thomasvendetta.com>
2012-07-26 18:27:17 -0400

    Mad changes

--- db/migrate/20120726215446_add_home_page_id_to_website.rb
+++ db/migrate/20120726215446_add_home_page_id_to_website.rb
@@ -1,5 +1,8 @@
 class AddHomePageIdToWebsite < ActiveRecord::Migration
-  def change
-    add_column :websites, :homepage_id, :reference
+  def up
+    add_column :websites, :homepage_id, :integer
+  end
+  def down
+    remove_column :websites, :homepage_id
   end
 end
