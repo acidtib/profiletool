74f9470e11cd6421f6123a7d82b9fa21a0ce94d4
Thomas Vendetta <thomas@thomasvendetta.com>
2012-07-26 20:04:11 -0400

    Added index

--- app/models/website.rb
+++ app/models/website.rb
@@ -2,17 +2,4 @@ class Website < ActiveRecord::Base
   belongs_to :user
   attr_accessible :domain, :website_pages_enabled
   has_many :website_pages
-  
-  def homepage
-    if self.homepage_id
-      self.website_pages.find(self.homepage_id)
-    else
-      nil
-    end
-  end
-
-  def homepage=(page)
-    self.homepage_id = page.id
-  end
-
-end
+ end
--- app/models/website_page.rb
+++ app/models/website_page.rb
@@ -1,23 +1,7 @@
 class WebsitePage < ActiveRecord::Base
   belongs_to :website
   attr_accessible :body_html, :body_markdown, :path, :title, :is_homepage
-  attr_accessor :is_homepage
+  validates_presence_of :title, :path, :body_markdown
+  validates_uniqueness_of :path, :scope => [ :website_id ]
 
-  before_save :update_homepage
-
-  def is_homepage
-    @homepage
-  end
-
-  def is_homepage=(bool)
-    @homepage = bool
-  end
-
-  def update_homepage
-    if self.is_homepage
-      website = self.website
-      website.homepage=(self)
-      website.save
-    end
-  end
 end
